6b192bcb0e7e1fdb503b032aea9eb06c83d6d719
Cameron Jensen <jensenseanc@gmail.com>
2017-03-03 09:02:07 -0800

    Add value for each topping to be add to the constructor using a prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,58 @@
 //Business Logic
-function Pizza () {
-  this.topping = [cheese, pepperoni, artichoke, chicken, sausage, pineapple, bacon];
-  this.circumference = [10, 15, 20];
+function Pizza (circumference) {
+  this.circumference = circumference;
+  this.topping = 0;
   this.toppingTotal = 0;
   this.circumferenceTotal = 0;
 }
 
-Pizza.prototype.addTopping = function () {
-  (add $1 per topping);
+Pizza.prototype.addToppingOne = function () {
+  this.topping += 1;
 }
 
-Pizza.prototype.smallSize = function () {
-  ($10);
+Pizza.prototype.addToppingTwo = function () {
+  this.topping += 2;
 }
-
-Pizza.prototype.mediumSize = function () {
-  ($15);
-}
-
-Pizza.prototype.largeSize = function () {
-  ($20);
-}
-
-Pizza.prototype.costOfPizza = function () {
-  (add $1 per topping);
-}
+//
+// Pizza.prototype.smallSize = function () {
+//   ($10);
+// }
+//
+// Pizza.prototype.mediumSize = function () {
+//   ($15);
+// }
+//
+// Pizza.prototype.largeSize = function () {
+//   ($20);
+// }
+//
+// Pizza.prototype.costOfPizza = function () {
+//   (add $1 per topping);
+// }
 
 
 //User Interface
+$(function () {
+  $("#pizzaSelections").submit(function () {
+    event.preventDefault();
+
+    var circumference = $("input:radio[name=pizzaSize]:checked").val();
+
+    var pricingPizza = new Pizza (circumference);
+
+    $("input:checkbox[name=pizza-topping]:checked").each(function(){
+      var topping = parseInt($(this).val());
+      if (topping === 1) {
+        pricingPizza.addToppingOne();
+      } else if (topping === 2) {
+        pricingPizza.addToppingTwo();
+      } 
+    });
+
+
+
+
+
+    console.log(pricingPizza);
+  });
+});
