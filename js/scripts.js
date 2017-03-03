a9a157a82f2b9d7a3f8d7d256df2ecc390adcf1b
Cameron Jensen <jensenseanc@gmail.com>
2017-03-03 13:43:34 -0800

    Add ability to order multiple pizzas at one time. Add Name and Phone number as inputs that are added to the Pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //Business Logic
-function Pizza (circumference) {
-  this.circumference = circumference;
+function Pizza (firstName, phoneNumber) {
+  this.firstName = firstName;
+  this.phoneNumber = phoneNumber;
   this.circumferenceTotal = 0;
   this.topping = 0;
   this.totalPrice = 0;
@@ -19,11 +20,11 @@ Pizza.prototype.smallSize = function () {
 }
 
 Pizza.prototype.mediumSize = function () {
-  this.circumferenceTotal += 15;
+  this.circumferenceTotal += 17;
 }
 
 Pizza.prototype.largeSize = function () {
-  this.circumferenceTotal += 20;
+  this.circumferenceTotal += 25;
 }
 
 Pizza.prototype.costOfPizza = function () {
@@ -34,47 +35,63 @@ Pizza.prototype.costOfPizza = function () {
 //User Interface
 $(function () {
 
-  // $("#add-pizza").click(function() {
-  //   $("#otherPizza").append('<div class="form-group">' +
-  //                               '<h2>Select Size of Pizza</h2>' +
-  //                               '<div class="radio">' +
-  //                                   '<label><input type="radio" name="pizzaSize" value="sm">Personal</label>' +
-  //                               '</div>' +
-  //                               '<div class="radio">' +
-  //                                 '<label><input type="radio" name="pizzaSize" value="med">Medium</label>' +
-  //                               '</div>' +
-  //                               '<div class="radio">' +
-  //                                 '<label><input type="radio" name="pizzaSize" value="lrg">Large</label>' +
-  //                               '</div>' +
-  //                             '</div>' +
-  //                             '<div class="form-group">' +
-  //                               '<p>What Type of Topping would you like:</p>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="1">Pepperoni<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="2">Sausage<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="1">Extra Cheese<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="1">Bacon<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="2">Pineapple<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="1">Spinach<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="1">Onions<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="2">Ham<br>' +
-  //                               '<input type="checkbox" name="pizza-topping" value="1">Mushrooms<br>' +
-  //                             '</div>');
-  // });
+  $("#add-pizza").click(function() {
+    $("#otherPizza").append('<div class="col-md-3">' +
+                              '<div class="form-group">' +
+                                '<div class="panel panel-success">' +
+                                  '<div class="panel-heading">' +
+                                    '<h2 class="panel-title">Select Size of Pizza</h2>' +
+                                  '</div>' +
+                                  '<div class="panel-body">' +
+                              '<input type="checkbox" name="pizza-size" value="10"> Small<br>' +
+                              '<input type="checkbox" name="pizza-size" value="17"> Medium<br>' +
+                              '<input type="checkbox" name="pizza-size" value="25"> Large<br>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '<div class="col-md-3">' +
+                              '<div class="form-group">' +
+                              '<div class="panel panel-success">' +
+                                '<div class="panel-heading">' +
+                                  '<p class="panel-title">Select Toppings:</p>' +
+                                '</div>' +
+                                '<div class="panel-body">' +
+                                '<input type="checkbox" name="pizza-topping" value="1">Pepperoni<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="2">Sausage<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="1">Extra Cheese<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="1">Bacon<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="2">Pineapple<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="1">Spinach<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="1">Onions<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="2">Ham<br>' +
+                                '<input type="checkbox" name="pizza-topping" value="1">Mushrooms<br>' +
+                              '</div>' +
+                              '</div>' +
+                              '</div>' +
+                              '</div>');
+  });
 
   $("#pizzaSelections").submit(function () {
     event.preventDefault();
 
-    var circumference = $("input:radio[name=pizzaSize]:checked").val();
 
-    var pricingPizza = new Pizza (circumference);
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedPhoneNumber = $("input#new-phone").val();
 
-    if (circumference === "sm") {
-      pricingPizza.smallSize();
-    } else if (circumference === "med") {
-      pricingPizza.mediumSize();
-    } else if (circumference === "lrg") {
-      pricingPizza.largeSize();
-    }
+    var pricingPizza = new Pizza (inputtedFirstName, inputtedPhoneNumber);
+
+
+    $("input:checkbox[name=pizza-size]:checked").each(function(){
+      var topping = parseInt($(this).val());
+      if (topping === 10) {
+        pricingPizza.smallSize();
+      } else if (topping === 17) {
+        pricingPizza.mediumSize();
+      } else if (topping === 25){
+        pricingPizza.largeSize();
+      }
+    });
 
     $("input:checkbox[name=pizza-topping]:checked").each(function(){
       var topping = parseInt($(this).val());
@@ -87,15 +104,7 @@ $(function () {
 
     pricingPizza.costOfPizza();
 
-    $("#showTotal").html("<h3> $" + pricingPizza.totalPrice + "</h3>");
-    $("#navbarTotal").html("$" + pricingPizza.totalPrice);
+    $("#navbarTotal").html("  " + pricingPizza.firstName + " your total is $" + pricingPizza.totalPrice);
     console.log(pricingPizza);
   });
 });
-
-
-                        // '<select class="form-control" id="pizzaSizeDrop">' +
-                        //   '<option value="sm">Personal</option>' +
-                        //   '<option value="med">Medium</option>' +
-                        //   '<option value="lrg">Large</option>' +
-                        // '</select>'
