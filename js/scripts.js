651dacda82d2124c6a23aae4207ad9932c5bf9fb
Cameron Jensen <jensenseanc@gmail.com>
2017-03-03 09:13:13 -0800

    Add value for pizza size to be added to costructor using multiple prototypes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 //Business Logic
 function Pizza (circumference) {
   this.circumference = circumference;
-  this.topping = 0;
-  this.toppingTotal = 0;
   this.circumferenceTotal = 0;
+  this.topping = 0;
 }
 
 Pizza.prototype.addToppingOne = function () {
@@ -13,19 +12,19 @@ Pizza.prototype.addToppingOne = function () {
 Pizza.prototype.addToppingTwo = function () {
   this.topping += 2;
 }
-//
-// Pizza.prototype.smallSize = function () {
-//   ($10);
-// }
-//
-// Pizza.prototype.mediumSize = function () {
-//   ($15);
-// }
-//
-// Pizza.prototype.largeSize = function () {
-//   ($20);
-// }
-//
+
+Pizza.prototype.smallSize = function () {
+  this.circumferenceTotal += 10;
+}
+
+Pizza.prototype.mediumSize = function () {
+  this.circumferenceTotal += 15;
+}
+
+Pizza.prototype.largeSize = function () {
+  this.circumferenceTotal += 20;
+}
+
 // Pizza.prototype.costOfPizza = function () {
 //   (add $1 per topping);
 // }
@@ -40,13 +39,22 @@ $(function () {
 
     var pricingPizza = new Pizza (circumference);
 
+    if (circumference === "sm") {
+      pricingPizza.smallSize();
+    } else if (circumference === "med") {
+      pricingPizza.mediumSize();
+    } else if (circumference === "lrg") {
+      pricingPizza.largeSize();
+    }
+
+
     $("input:checkbox[name=pizza-topping]:checked").each(function(){
       var topping = parseInt($(this).val());
       if (topping === 1) {
         pricingPizza.addToppingOne();
       } else if (topping === 2) {
         pricingPizza.addToppingTwo();
-      } 
+      }
     });
 
 
