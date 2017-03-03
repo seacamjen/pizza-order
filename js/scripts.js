bd44e31018311e402f774fd19339ff9241c93102
Cameron Jensen <jensenseanc@gmail.com>
2017-03-03 08:23:01 -0800

    Add initial Pizza object and prototypes for each topping, size of pizza and cost of pizza.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+//Business Logic
+function Pizza () {
+  this.topping = [cheese, pepperoni, artichoke, chicken, sausage, pineapple, bacon];
+  this.circumference = [10, 15, 20];
+  this.toppingTotal = 0;
+  this.circumferenceTotal = 0;
+}
+
+Pizza.prototype.addTopping = function () {
+  (add $1 per topping);
+}
+
+Pizza.prototype.smallSize = function () {
+  ($10);
+}
+
+Pizza.prototype.mediumSize = function () {
+  ($15);
+}
+
+Pizza.prototype.largeSize = function () {
+  ($20);
+}
+
+Pizza.prototype.costOfPizza = function () {
+  (add $1 per topping);
+}
+
+
+//User Interface
